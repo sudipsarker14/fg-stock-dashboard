91a9163939f24e97671a7ca51ce09d174db60dcf
Sudip Sarker <sudipsarker2016@gmail.com>
2026-02-02 18:29:27 +0600

    Add SQL query for item-wise balance and value

---
+++ item_wise.sql
@@ -0,0 +1,40 @@
+SELECT
+    od.fg_categ_type AS "Item",
+
+    -- FG Balance by Aging Bucket
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 0 AND 5 THEN od.fg_balance ELSE 0 END) AS "Balance_0-5",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 6 AND 10 THEN od.fg_balance ELSE 0 END) AS "Balance_6-10",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 11 AND 15 THEN od.fg_balance ELSE 0 END) AS "Balance_11-15",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 16 AND 20 THEN od.fg_balance ELSE 0 END) AS "Balance_16-20",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 21 AND 25 THEN od.fg_balance ELSE 0 END) AS "Balance_21-25",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 26 AND 30 THEN od.fg_balance ELSE 0 END) AS "Balance_26-30",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) > 30 THEN od.fg_balance ELSE 0 END) AS "Balance_30+",
+
+    -- Value by Aging Bucket
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 0 AND 5 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_0-5",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 6 AND 10 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_6-10",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 11 AND 15 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_11-15",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 16 AND 20 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_16-20",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 21 AND 25 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_21-25",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) BETWEEN 26 AND 30 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_26-30",
+    SUM(CASE WHEN (CURRENT_DATE - od.action_date::date) > 30 THEN od.fg_balance * od.price_unit ELSE 0 END) AS "Value_30+",
+
+    -- Grand totals
+    SUM(od.fg_balance) AS "Total_Balance",
+    SUM(od.fg_balance * od.price_unit) AS "Total_Value"
+
+FROM operation_details od
+
+LEFT JOIN sale_order so
+       ON od.oa_id = so.id
+
+LEFT JOIN account_payment_term apt
+       ON so.payment_term_id = apt.id
+
+WHERE
+      od.next_operation = 'FG Packing'
+  AND od.state NOT IN ('done', 'closed')
+  AND od.fg_balance > 0
+
+GROUP BY COALESCE(apt.name ->> 'en_US', apt.name::text), od.fg_categ_type
+ORDER BY "Item";
